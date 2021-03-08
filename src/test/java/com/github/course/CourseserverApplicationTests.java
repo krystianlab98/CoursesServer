5a2cbee5d2c9396ca7d6fabfcbcdd5d70f0af286
krystianlab98 <krystiablab98@gmail.com>
2021-03-08 15:48:02 +0100

    "init commit"

---
+++ src/test/java/com/github/course/CourseserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
