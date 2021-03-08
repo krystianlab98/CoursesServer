a30a20dfedb06956bb2d6d4a21960c38b69ee846
krystianlab98 <krystiablab98@gmail.com>
2021-03-08 16:50:36 +0100

    "Added Course entity"

---
+++ src/main/java/com/github/course/features/course/Course.java
@@ -0,0 +1,34 @@
+package com.github.course.features.course;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+
+@Getter
+@Setter
+@Entity
+@Table(name = "courses")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name="title")
+    private String title;
+
+    @Column
+    private String description;
+
+    public Course(Long id, String title, String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+
+    public Course() {
+    }
+}
+
