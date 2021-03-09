ea9d9c042508162450ac4d7c24df0a8a08dfc08c
krystianlab98 <krystiablab98@gmail.com>
2021-03-09 21:04:26 +0100

    "Added web layer - Rest methods for category entity"

--- src/main/java/com/github/course/CourseserverApplication.java
+++ src/main/java/com/github/course/CourseserverApplication.java
@@ -1,5 +1,7 @@
 package com.github.course;
 
+import com.github.course.features.category.Category;
+import com.github.course.features.category.CategoryDao;
 import com.github.course.features.course.Course;
 import com.github.course.features.course.CourseDao;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @SpringBootApplication
 public class CourseserverApplication {
@@ -17,8 +22,9 @@ public class CourseserverApplication {
     }
 
     @Bean
-    public CommandLineRunner insertDbRecords(@Autowired CourseDao courseDao) {
+    public CommandLineRunner insertDbRecords(@Autowired CourseDao courseDao, @Autowired CategoryDao categoryDao) {
         return (args) -> {
+
             Course course = new Course();
             course.setTitle("Java Course");
             course.setDescription("Complete course for java dev");
@@ -28,6 +34,27 @@ public class CourseserverApplication {
             course2.setTitle("Python Course");
             course2.setDescription("Complete course for python developers");
             courseDao.save(course2);
+
+            Course course3 = new Course();
+            course3.setTitle("JavaScript Course");
+            course3.setDescription("Complete course for fronted devs ");
+            courseDao.save(course3);
+
+            Category category = new Category();
+            category.setTitle("backend programing");
+            Set<Course> courses = new HashSet<>();
+            courses.add(course);
+            courses.add(course2);
+            category.setCourses(courses);
+            categoryDao.save(category);
+
+            Category category2 = new Category();
+            category2.setTitle("frontend programing");
+            Set<Course> courses2 = new HashSet<>();
+            courses2.add(course3);
+            category2.setCourses(courses2);
+            categoryDao.save(category2);
+
         };
 
     }
---
+++ src/main/java/com/github/course/features/category/CategoryServiceImpl.java
@@ -0,0 +1,67 @@
+package com.github.course.features.category;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+    private CategoryDao categoryDao;
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    public CategoryServiceImpl(CategoryRepository categoryRepository, CategoryDao categoryDao) {
+        this.categoryDao = categoryDao;
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public List<Category> findAllCategories() {
+        return categoryDao.findAllCategories();
+    }
+
+    @Override
+    public Category findCategoryById(Long id) {
+        return categoryDao.findCategoryById(id).get();
+    }
+
+    @Override
+    public List<Category> findCategoryByTitle(String title) {
+        List<Category> titles = categoryDao.findAllCategories()
+                .stream()
+                .map(category -> findInString(title, category.getTitle()))
+                .filter(str -> str != null)
+                .map(finded -> categoryDao.findCategoryByTitle(finded))
+                .collect(Collectors.toList());
+
+        return titles;
+    }
+
+    @Override
+    public void add(Category category) {
+        categoryDao.save(category);
+    }
+
+    //chwilowy brak możliwości edycji kursów
+    @Override
+    public void update(Category newCategory, Long id) {
+        categoryRepository.findById(id)
+                .map(category -> {
+                    category.setTitle(newCategory.getTitle());
+                    //category.setCourses(newCategory.getCourses());
+                    return categoryRepository.save(category);
+                }).orElseThrow();
+    }
+
+    @Override
+    public void delete(Long id) {
+        categoryDao.delete(id);
+    }
+
+    private String findInString(String word, String text) {
+        return text.toLowerCase().indexOf(word.toLowerCase()) > -1 ? text : null;
+    }
+}
