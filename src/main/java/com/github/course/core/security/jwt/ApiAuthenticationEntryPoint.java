f690da5756443efcd36d6608851d2a4bb60c3b5c
krystianlab98 <krystiablab98@gmail.com>
2021-03-28 11:45:48 +0200

    "Added JWT token util and filter"

---
+++ src/main/java/com/github/course/core/security/jwt/ApiAuthenticationEntryPoint.java
@@ -0,0 +1,18 @@
+package com.github.course.core.security.jwt;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ApiAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+                         AuthenticationException authenticationException) throws IOException, ServletException {
+
+        httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+    }
+}
