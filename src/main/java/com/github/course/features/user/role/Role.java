37fc0ef321382a0ea1d245a550213a7a38a954be
krystianlab98 <krystiablab98@gmail.com>
2021-03-29 23:03:20 +0200

    "Added Role repository and DAO"

--- src/main/java/com/github/course/features/user/role/Role.java
+++ src/main/java/com/github/course/features/user/role/Role.java
@@ -1,4 +1,4 @@
-package com.github.course.features.user;
+package com.github.course.features.user.role;
 
 import lombok.Getter;
 import lombok.Setter;
---
+++ src/main/java/com/github/course/features/user/role/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.github.course.features.user.role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Role findByRole(String role);
+}
